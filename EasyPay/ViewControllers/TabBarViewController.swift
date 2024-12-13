dea5f341262e933d413ed6b8a707514f39042737
Vaishnavi Jothikumar <vaishnavijothikumar@gmail.com>
2024-12-13 17:55:56 +0530

    Tiny Corrections

--- EasyPay/ViewControllers/TabBarViewController.swift
+++ EasyPay/ViewControllers/TabBarViewController.swift
@@ -11,5 +11,18 @@ class TabBarViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        if let tabBar = self.tabBarController?.tabBar {
+                var frame = tabBar.frame
+
+                // Set height based on device type
+                if UIDevice.current.userInterfaceIdiom == .phone {
+                    frame.size.height = 50 // Height for iPhone
+                } else if UIDevice.current.userInterfaceIdiom == .pad {
+                    frame.size.height = 200// Height for iPad
+                }
+                
+                frame.origin.y = self.view.frame.size.height - frame.size.height
+                tabBar.frame = frame
+            }
     }
 }
